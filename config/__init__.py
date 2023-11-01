871c00b07e96da4eaee2982c7d6a124a8a68da4a
Daniel CAZABAT <dacazabatqgmail.com>
2023-11-01 08:06:20 -0300

    DB SQLite

--- config/__init__.py
+++ config/__init__.py
@@ -7,14 +7,14 @@ load_dotenv()
 # I configure the database engine and the connection string
 
 # With SQLite
-# STRCNX='sqlite:///mydb.db'
+STRCNX='sqlite:///mydb.db'
 
-ENGINE=os.getenv('ENGINE')
-HOST= os.getenv('HOST')
-USERDB= os.getenv('USERDB')
-PWDS= os.getenv('PWDS')
-DBA= os.getenv('DBA')
-PORT= os.getenv('PORT')
+# ENGINE=os.getenv('ENGINE')
+# HOST= os.getenv('HOST')
+# USERDB= os.getenv('USERDB')
+# PWDS= os.getenv('PWDS')
+# DBA= os.getenv('DBA')
+# PORT= os.getenv('PORT')
 
-STRCNX=f'{ENGINE}://{USERDB}:{PWDS}@{HOST}:{PORT}/{DBA}'
+# STRCNX=f'{ENGINE}://{USERDB}:{PWDS}@{HOST}:{PORT}/{DBA}'
 SQLALCHEMY_DATABASE_URI=STRCNX
